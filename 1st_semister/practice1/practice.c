c28351c32a820a1e3fa923aa19add0eb168d7bfa
Md. Hasan Mia <hasanrafi69@gmail.com>
2024-04-02 22:53:38 +0600

    solve practice 1

---
+++ 1st_semister/practice1/practice.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+int main(){
+    
+    // =======Problem No: 01 ======
+    // printf("Recently I heard that you’ve achieved 95%% marks in your exam.\nThis is brilliant! \nI wish you’ll shine in your life!	Good luck with all the barriers(/\\) in your life.");
+    
+    // =======Problem No: 02 ======
+    /* 
+    You need to take two integer values as input and show the summation, subtraction, multiplication and division in the given format below.
+    For example:
+    Inputs are 5 and 2
+    Then you’ll give output as:
+    5 + 2 = 7
+    5 - 2 = 3
+    5 * 2 = 10
+    5 / 2 = 2.50
+    */
+
+    // ======== solution ============
+
+    // int a, b;
+
+    // printf("Enter first number: ");
+    // scanf("%d", &a);
+
+    // printf("Enter second number: ");
+    // scanf("%d", &b);
+    // int sum = a + b;
+    // int dif = a - b;
+    // int mul = a*b;
+    // float divi = (float)a/b;
+
+    // printf("%d + %d = %d\n", a, b, sum);
+    // printf("%d - %d = %d\n", a, b, dif);
+    // printf("%d * %d = %d\n", a , b, mul);
+    // printf("%d / %d = %.2f\n", a, b, divi);
+
+
+    // =======Problem No: 03 ======
+    /* 
+    You need to take one non-negative integer (0 or greater than 0) value as input and tell if it is even or odd.
+
+    Then you’ll give output as:
+    10 even
+    3 odd
+    */
+
+    // ======== solution ============
+
+    // int a;
+    
+    // printf("Enter number: ");
+    
+    // scanf("%d", &a);
+    
+    // if ( a % 2 == 0)
+    // {
+    //     printf("even");
+    // }else{
+    //     printf("odd");
+    // };
+
+    // =======Problem No: 04 ======
+    /* 
+    You need to take one integer value as input and tell if the value is positive or negative or zero.
+    See the sample input and output for more clarification.
+    Then you’ll give output as:
+    10 positive
+    -50 negative
+    0 zero
+    */
+
+    // ======== solution ============
+    // int a;
+    // printf("Enter number: ");
+    // scanf("%d", &a);
+    // if(a > 0){
+    //     printf("positive");
+    // }else if(a < 0){
+    //     printf("negative");
+    // }else{
+    //     printf("zero");
+    // };
+
+    // =======Problem No: 05 ======
+    /* 
+    Suppose your friend has told you that she will buy a Gucci Bag if she has 10 thousand taka or more. Otherwise if she has 5 thousand taka or more, she will buy a Levis Bag. Otherwise she will buy Something from New Market. She also told you that, if she could buy the Gucci bag and she has more than 20 thousand taka she will also buy a Gucci Belt.
+
+    Then you’ll give output as:
+    20000 Gucci Bag
+    6500 Levis Bag
+    200 Something
+    25400  Gucci Bag Gucci Belt
+    */
+
+    // ======== solution ============
+    // int a;
+    // printf("Enter number: ");
+    // scanf("%d", &a);
+    // if(a > 20000){
+    //     printf("Gucci Bag Gucci Belt");
+    // }else if(a > 10000 && a <= 20000){
+    //     printf("Gucci Bag");
+    // }else if(a >= 5000){
+    //     printf("Levis Bag");
+    // }else{
+    //     printf("Something");
+    // };
+    
+    return 0;
+}
